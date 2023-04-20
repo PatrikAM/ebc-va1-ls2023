85cbffe34e8ac305170e799d35e22c3e5e6c8eef
xmichl <xmichl@mendelu.cz>
2023-04-20 14:31:21 +0200

    colors extracted

--- app/src/main/java/cz/mendelu/pef/va1/xmichl/homework2/ui/elements/LetterAvatar.kt
+++ app/src/main/java/cz/mendelu/pef/va1/xmichl/homework2/ui/elements/LetterAvatar.kt
@@ -12,24 +12,17 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import cz.mendelu.pef.va1.xmichl.homework2.ui.theme.AvatarColors
 import kotlin.random.Random
 
 @Composable
 fun LetterAvatar(letter: Char) {
-    val colors: List<Color> = listOf(
-        Color(0xffa05203),
-        Color(0xff034f22),
-        Color(0xff15157a),
-        Color(0xFF7A0010),
-        Color(0xFF40270B),
-        Color(0xFF88304a)
-    )
     Box(
         contentAlignment = Alignment.Center,
         modifier = Modifier
             .size(50.dp)
             .background(
-                colors[Random.nextInt(0, colors.size-1)],
+                AvatarColors[Random.nextInt(0, AvatarColors.size-1)],
                 shape = CircleShape
             )
     ) {
--- app/src/main/java/cz/mendelu/pef/va1/xmichl/homework2/ui/theme/Color.kt
+++ app/src/main/java/cz/mendelu/pef/va1/xmichl/homework2/ui/theme/Color.kt
@@ -9,3 +9,12 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val AvatarColors: List<Color> = listOf(
+    Color(0xffa05203),
+    Color(0xff034f22),
+    Color(0xff15157a),
+    Color(0xFF7A0010),
+    Color(0xFF40270B),
+    Color(0xFF88304a)
+)
